b68a93b5ea10daf2f191c3aa79f20b2f44726f71
Carol-gh <quintanavargascarolselena@gmail.com>
2023-11-04 16:30:15 -0400

    actualizado

--- resources/views/evaluaciones/evaluar.blade.php
+++ resources/views/evaluaciones/evaluar.blade.php
@@ -278,7 +278,7 @@
     });
 
   function sendImageUrlToEndpoint(imageUrl) {
-  fetch('http://127.0.0.1:5000/predict_emotion', {
+  fetch('https://emocionweb.online/predict_emotion', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
